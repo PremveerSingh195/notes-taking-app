88e7a4255a676ba41738a51e144214cf7bf9f65a
callsmasterofficial <partnerchc@outlook.com>
2024-08-06 18:51:53 +0530

    dsgv

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,52 +1,83 @@
-"use client"
-import React, { useState , useEffect } from 'react'
-import axios from 'axios'
-import {toast} from 'react-hot-toast'
-import { useRouter } from 'next/navigation'
-
-
+"use client";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 function page() {
-const router = useRouter()
-   const [user , setUser] = useState({
-    email:"",
-    password:"",
-    username : ""
-   })
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-   const [buttonDisabled , setButtonDisabled] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
-   const [loading , setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-   const onSignup = async () => {
+  const onSignup = async () => {
     try {
-      setLoading (true)
-     const response =  await axios.post("/api/user/signup" , user)
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
 
-     console.log("Signup success" , response.data);
-    
-     router.push('/login')
+      console.log("Signup success", response.data);
 
-    } catch (error : any) {
+      router.push("/login");
+    } catch (error: any) {
       console.log("signup failed");
-      toast.error(error.message)
+      toast.error(error.message);
     }
-   }
+  };
 
-   useEffect(()=> {
-        if (user.email.length > 0 && user.password.length > 0 && user.password.length > 0 && user.username.length > 0)  {
-          setButtonDisabled(false)
-        } else {
-          setButtonDisabled(true)
-        }
-     } , [user])
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.password.length > 0 
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
-    <div className='bg-slate-400 w-screen h-screen flex justify-center items-center '>
-        <div className='bg-blue-500 h-auto w-auto'>
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-600">
+      <h1>{loading ? "Processing" : "Signup"}</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input 
+      className=""
+      id="username"
+      value={user.username}
+      onChange={(e)=>setUser({...user, username:e.target.value})}
+      placeholder="username"
+      type="text" />
+      <label htmlFor="email">Email</label>
+      <input 
+      className=""
+      id="email"
+      value={user.email}
+      onChange={(e)=>setUser({...user, email:e.target.value})}
+      placeholder="email"
+      type="text" />
+       <label htmlFor="password">Password</label>
+      <input 
+      className=""
+      id="password"
+      value={user.password}
+      onChange={(e)=>setUser({...user, password:e.target.value})}
+      placeholder="password"
+      type="password" />
+
+      <button onClick={onSignup}>
+        {buttonDisabled ? "no signup" : "Signup"}
+      </button>
+      <Link href="/login">Visit Login Page</Link>    
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React, { useState } from 'react'
+
+
+function page() {
+
+const [token , setToken] = useState("")
+
+const [verified, setVerified] = useState(false)
+
+const [error , setError] = useState(false)
+
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
